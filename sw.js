c12c507c9faa032f7e193983fd64dc090fad072b
Eymistaken <110712235+Eymistaken@users.noreply.github.com>
2026-01-20 23:26:02 +0300

    Update service worker to version 6

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'blink-counter-v4'; // GÜNCELLEME İÇİN BURAYI DEĞİŞTİR (v3, v4, v5...)
+const CACHE_NAME = 'blink-counter-v6'; // GÜNCELLEME İÇİN BURAYI DEĞİŞTİR (v3, v4, v5...)
 const urlsToCache = [
   './',
   './index.html',
@@ -66,4 +66,5 @@ self.addEventListener('activate', (event) => {
     })
   );
 });
+
 
